662ad124e47a487ab4352e06f94ab9c8c76ce699
llnnn-ux <72482919+llnnn-ux@users.noreply.github.com>
2021-01-12 02:07:35 +0900

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env sh
 adb shell pm uninstall --user 0 com.nttdocomo.android.osv
 adb shell pm uninstall --user 0 com.sonymobile.entrance
 adb shell pm uninstall --user 0 com.touchtype.swiftkey
