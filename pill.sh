b577175b5107a04ae75e67c7835699d1fa39c616
u0_a266 <72482919+u0a266@users.noreply.github.com>
2021-03-15 21:24:32 +0900

    Update pill.sh

--- pill.sh
+++ pill.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+#https://forum.xda-developers.com/t/pie-port-non-root-pill-xperia-10-and-10-plus-launcher-fo-xperia-xa2-pie.3905699/
 adb install XperiaLauncher.apk
 adb install WallpaperPicker.apk
 adb shell settings put secure swipe_up_to_switch_apps_enabled 1
